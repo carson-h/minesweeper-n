2d13897819ce22c2448901b9c1148e5f6fb3b3f0
Carson Hall <51799808+carson-h@users.noreply.github.com>
2023-05-16 00:15:43 -0400

    Sequential test server and no-modifications testing process.

--- lib/minesweeper.ex
+++ lib/minesweeper.ex
@@ -6,11 +6,12 @@ defmodule Minesweeper do
   import MsprSolve
 
   @doc """
-  Testing service for Minesweeper solver.
+  Testing service for Minesweeper solver [concurrent testing].
 
   `dim` is a tuple representing the dimensions of the boards to be tested.
   `num` is the number of mines to be placed on the board.
   `count` is the number of times to repeat this test.
+  `verbose` determines whether to print the results of the tests to console.
 
   Returns a tuple of the form {wins, losses}.
   """
@@ -62,6 +63,68 @@ defmodule Minesweeper do
                   num - (n_acts |> Enum.count(fn x -> elem(x, 0) == :flag end)), # Remove new mine explorations
                   n_acts,
                   verbose) 
+      end
+    else # Game loss on unsafe move
+      if verbose, do: IO.inspect("Explored Bomb")
+      send(parent, :failure)
+    end
+  end
+
+  @doc """
+  Testing service for Minesweeper solver [sequential testing].
+
+  `dim` is a tuple representing the dimensions of the boards to be tested.
+  `num` is the number of mines to be placed on the board.
+  `count` is the number of times to repeat this test.
+  `verbose` determines whether to print the results of the tests to console.
+
+  Returns a tuple of the form {wins, losses}.
+  """
+  def test_solve_seq(dim, num, count, verbose \\ false), do: test_solve_seq(dim, num, count, {0, 0}, verbose)
+  defp test_solve_seq(_, _, 0, res, _), do: res # Return result when all tests completed.
+  defp test_solve_seq(dim, num, count, {win, loss}, verbose) do
+    chal_serv_nomod(self(), dim, num, verbose) # Run challenge
+    receive do # Update results, and proceed with testing
+      :success -> test_solve_seq(dim, num, count-1, {win+1, loss}, verbose)
+      :failure -> test_solve_seq(dim, num, count-1, {win, loss+1}, verbose)
+    end
+  end
+
+  @doc """
+  Testing server process.
+
+  `parent` is the process ID of the parent process.
+  `dim` is a tuple representing the dimensions of the boards to be tested.
+  `num` is the number of mines to be placed on the board.
+  `verbose` determines whether to print the results of the tests to console.
+
+  Returns either `:success` or `:failure` depending on the results of the test.
+  """
+  def chal_serv_nomod(parent, dim, num, verbose) do
+    {b, f} = gen_chal(dim, num) # Generate challenge board, and accompanying reference
+    chal_serv_nomod(parent, b, f, num, [{:nil, {}}], verbose)  # Begin with no initial actions
+  end
+  defp chal_serv_nomod(parent, _, _, _, [], verbose) do # No actions take on previous step
+    if verbose, do: IO.inspect("Stuck")
+    send(parent, :failure) # Got stuck
+  end
+  defp chal_serv_nomod(parent, _, _, _, [[:error]], verbose) do # No valid action discovered
+    if verbose, do: IO.inspect("Search Error")
+    send(parent, :failure) # Can't find valid solution
+  end
+  defp chal_serv_nomod(parent, ref, field, num, acts, verbose) do
+    if safe_acts?(ref, acts) do # Only proceed if no bomb has been explored
+      if solved?(field |> apply_acts(ref, acts), ref) do # Stop if board is solved
+        if verbose, do: IO.inspect("Success")
+        send(parent, :success)
+      else # Unsolved, continue search
+        n_acts = solve(field |> apply_acts(ref, acts), num) # Find new actions
+        chal_serv_nomod(parent,
+                        ref,
+                        field, # Apply actions to field
+                        num - (n_acts |> Enum.count(fn x -> elem(x, 0) == :flag end)), # Remove new mine explorations
+                        n_acts ++ acts,
+                        verbose) 
       end
     else # Game loss on unsafe move
       if verbose, do: IO.inspect("Explored Bomb")
