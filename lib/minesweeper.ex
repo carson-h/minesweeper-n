83c3addb27493a0f794db3a8e3c4ef23cf03e31a
Carson Hall <51799808+carson-h@users.noreply.github.com>
2023-05-17 03:24:17 -0400

    Added distant unexplored probability check. Fixed failure when no solutions found in prsearch.

--- lib/minesweeper.ex
+++ lib/minesweeper.ex
@@ -80,13 +80,13 @@ defmodule Minesweeper do
 
   Returns a tuple of the form {wins, losses}.
   """
-  def test_solve_seq(dim, num, count, verbose \\ false, proc_limit \\ -1), do: test_solve_seq(dim, num, count, {0, 0}, verbose, proc_limit)
+  def test_solve_seq(dim, num, count, proc_limit \\ -1, verbose \\ false), do: test_solve_seq(dim, num, count, {0, 0}, proc_limit, verbose)
   defp test_solve_seq(_, _, 0, res, _, _), do: res # Return result when all tests completed.
-  defp test_solve_seq(dim, num, count, {win, loss}, verbose, proc_limit) do
-    chal_serv_nomod(self(), dim, num, verbose, proc_limit) # Run challenge
+  defp test_solve_seq(dim, num, count, {win, loss}, proc_limit, verbose) do
+    chal_serv_nomod(self(), dim, num, proc_limit, verbose) # Run challenge
     receive do # Update results, and proceed with testing
-      :success -> test_solve_seq(dim, num, count-1, {win+1, loss}, verbose, proc_limit)
-      :failure -> test_solve_seq(dim, num, count-1, {win, loss+1}, verbose, proc_limit)
+      :success -> test_solve_seq(dim, num, count-1, {win+1, loss}, proc_limit, verbose)
+      :failure -> test_solve_seq(dim, num, count-1, {win, loss+1}, proc_limit, verbose)
     end
   end
 
@@ -100,19 +100,19 @@ defmodule Minesweeper do
 
   Returns either `:success` or `:failure` depending on the results of the test.
   """
-  def chal_serv_nomod(parent, dim, num, verbose, proc_limit \\ -1) do
+  def chal_serv_nomod(parent, dim, num, proc_limit \\ -1, verbose) do
     {b, f} = gen_chal(dim, num) # Generate challenge board, and accompanying reference
-    chal_serv_nomod(parent, b, f, num, [{:nil, {}}], verbose, proc_limit)  # Begin with no initial actions
+    chal_serv_nomod(parent, b, f, num, [{:nil, {}}], proc_limit, verbose)  # Begin with no initial actions
   end
-  defp chal_serv_nomod(parent, _, _, _, [], verbose, _) do # No actions take on previous step
+  defp chal_serv_nomod(parent, _, _, _, [], _, verbose) do # No actions take on previous step
     if verbose, do: IO.inspect("Stuck")
     send(parent, :failure) # Got stuck
   end
-  defp chal_serv_nomod(parent, _, _, _, [[:error]], verbose, _) do # No valid action discovered
+  defp chal_serv_nomod(parent, _, _, _, [[:error]], _, verbose) do # No valid action discovered
     if verbose, do: IO.inspect("Search Error")
     send(parent, :failure) # Can't find valid solution
   end
-  defp chal_serv_nomod(parent, ref, field, num, acts, verbose, proc_limit) do
+  defp chal_serv_nomod(parent, ref, field, num, acts, proc_limit, verbose) do
     if safe_acts?(ref, acts) do # Only proceed if no bomb has been explored
       if solved?(field |> apply_acts(ref, acts), ref) do # Stop if board is solved
         if verbose, do: IO.inspect("Success")
@@ -124,8 +124,8 @@ defmodule Minesweeper do
                         field, # Apply actions to field
                         num - (n_acts |> Enum.count(fn x -> elem(x, 0) == :flag end)), # Remove new mine explorations
                         n_acts ++ acts,
-                        verbose,
-                        proc_limit) 
+                        proc_limit,
+                        verbose) 
       end
     else # Game loss on unsafe move
       if verbose, do: IO.inspect("Explored Bomb")
